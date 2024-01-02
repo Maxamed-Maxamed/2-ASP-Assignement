47905bbf1f858c2eef4e54f921c48149f101ef2f
Maxamed Maxamed <20088349@mail.wit.ie>
2024-01-02 20:06:45 +0000

    Updated second assignment, fixed errors

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
-import genresRouter from './api/genres'
+import genresRouter from './api/genres';
 import './db';
-import './seedData'
+import './seedData';
 import usersRouter from './api/users';
 // import authenticate from './authenticate';
 import passport from './authenticate';
 
-const errHandler = (err, req, res, next) => {
+const errHandler = (err, req, res) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
-  if(process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     return res.status(500).send(`Something went wrong!`);
   }
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
 };
+
 
 dotenv.config();
 
